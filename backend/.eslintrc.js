048ca2619ee7e8f0e65f2d15b4856d8a2b35e13c
michael person <michael.person@cognitic.be>
2026-03-09 22:50:11 +0100

    change eslint config

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     ],
     parserOptions: {
         ecmaVersion: 'latest',
-        sourceType: 'module'
+        sourceType: 'script'
     },
     rules: {
         'no-undef': 'off',
